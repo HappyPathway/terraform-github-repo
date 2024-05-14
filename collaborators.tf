3724b99c1a24f5019d44209359d515a75b2973db
David Arnold <10138997+djaboxx@users.noreply.github.com>
2024-05-14 16:52:10 -0700

    Create collaborators.tf

---
+++ collaborators.tf
@@ -0,0 +1,7 @@
+# Add a collaborator to a repository
+resource "github_repository_collaborator" "collaborators" {
+  for_each = tomap(var.collaborators)
+  repository = github_repository.repo.name
+  username   = each.key
+  permission = each.value
+}
