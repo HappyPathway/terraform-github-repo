7d4a8beea3816da73f0848c27ebbf5b725030cd4
Dave Arnold <dave@roknsound.com>
2025-02-17 23:36:56 -0800

    Add SSH clone URL output to outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
 output "github_repo" {
   value = github_repository.repo
 }
+
+output "ssh_clone_url" {
+  description = "URL that can be provided to git clone to clone the repository via SSH"
+  value       = github_repository.repo.ssh_clone_url
+}
